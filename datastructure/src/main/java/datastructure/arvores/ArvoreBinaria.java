f1bbfbac7afdc19e5ab234e7fc3aae63e8099187
Danilo Takeo <danilot.kanizawa@gmail.com>
2025-03-28 09:49:37 -0300

    Implementação da árvore binária

---
+++ datastructure/src/main/java/datastructure/arvores/ArvoreBinaria.java
@@ -0,0 +1,62 @@
+package datastructure.arvores;
+
+class No {
+    int valor;
+    No esquerda, direita;
+
+    public No(int item) {
+        valor = item;
+        esquerda = direita = null;
+    }
+}
+
+public class ArvoreBinaria {
+    No raiz;
+
+    public ArvoreBinaria() {
+        raiz = null;
+    }
+
+    public void inserir(int valor) {
+        raiz = inserirRec(raiz, valor);
+    }
+
+    private No inserirRec(No raiz, int valor) {
+        if (raiz == null) {
+            raiz = new No(valor);
+            return raiz;
+        }
+        if (valor < raiz.valor) {
+            raiz.esquerda = inserirRec(raiz.esquerda, valor);
+        } else if (valor > raiz.valor) {
+            raiz.direita = inserirRec(raiz.direita, valor);
+        }
+        return raiz;
+    }
+
+    public void emOrdem() {
+        emOrdemRec(raiz);
+    }
+
+    private void emOrdemRec(No raiz) {
+        if (raiz != null) {
+            emOrdemRec(raiz.esquerda);
+            System.out.print(raiz.valor + " ");
+            emOrdemRec(raiz.direita);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        arvore.inserir(50);
+        arvore.inserir(30);
+        arvore.inserir(70);
+        arvore.inserir(20);
+        arvore.inserir(40);
+        arvore.inserir(60);
+        arvore.inserir(80);
+
+        System.out.println("Percurso em ordem:");
+        arvore.emOrdem();
+    }
+}
